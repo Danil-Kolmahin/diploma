cd2c35a7b701f183a6ccfb66f1e4ad7749ea0fca
Kolmagin Danil <dkolmagin@gmail.com>
2022-03-15 16:01:15 +0200

    graphql !!!!!!!!!!!

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -4,6 +4,9 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { environment } from '../environments/environment';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { GraphQLModule } from '@nestjs/graphql';
+import { AppResolver } from './app.resolver';
+import { ApolloDriver } from '@nestjs/apollo';
 
 @Module({
   imports: [
@@ -11,8 +14,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
       ...environment.connection,
       // entities: [UserEntity],
     }),
+    GraphQLModule.forRoot({
+      typePaths: ['./**/*.graphql'],
+      context: ({ res }) => ({ res }),
+      playground: true,
+      driver: ApolloDriver,
+    }),
   ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AppResolver],
 })
-export class AppModule {}
+export class AppModule {
+}
