47e1f8d034f9dc7cb939629c9502a7500e06ce64
Kolmagin Danil <dkolmagin@gmail.com>
2022-03-15 12:21:54 +0200

    db ++

--- apps/server/src/app/app.module.ts
+++ apps/server/src/app/app.module.ts
@@ -2,9 +2,16 @@ import { Module } from '@nestjs/common';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { environment } from '../environments/environment';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot({
+      ...environment.connection,
+      // entities: [UserEntity],
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
