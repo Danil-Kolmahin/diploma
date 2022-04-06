fa77109383f29c32fed349e303bb361d56d8e542
Kolmagin Danil <dkolmagin@gmail.com>
2022-04-06 19:15:43 +0300

    simple diff end?

--- apps/client/src/app/simpleDifference/simpleDifference.tsx
+++ apps/client/src/app/simpleDifference/simpleDifference.tsx
@@ -5,7 +5,7 @@ import {
   Center,
   Highlight,
   ActionIcon,
-  Tooltip,
+  Tooltip, Progress,
 } from '@mantine/core';
 import { MainLinks } from '../_mainLinks';
 import { Logo } from '../_logo';
@@ -72,102 +72,150 @@ export const SimpleDifference = ({ parsedCookie }: any) => {
         </Text>
       </Center></Grid.Col>
 
-        <Grid.Col span={1}><Center>
+      {isCompared ?
+        <><Grid.Col span={3}><Center>
+          <Text
+            component='span'
+            align='left'
+            // size='xl'
+            // weight={700}
+            color={theme.colorScheme === APP_THEMES.DARK ? '#fff' : '#000'}
+          >
+            Similarity:
+          </Text>
+        </Center></Grid.Col>
+          <Grid.Col span={6}>
+            <Progress
+              label={`${Math.floor(
+                highlight.join('').length / firstText.length * 100
+              )}%`}
+              value={highlight.join('').length / firstText.length * 100}
+              size={20}
+            />
+          </Grid.Col>
+          <Grid.Col span={3}><Center>
+            <Text
+              component='span'
+              align='left'
+              // size='xl'
+              // weight={700}
+              color={theme.colorScheme === APP_THEMES.DARK ? '#fff' : '#000'}
+            >
+              {`${highlight.join('').length} / ${firstText.length}`}
+            </Text>
+          </Center></Grid.Col></>
+        :
+        <><Grid.Col span={1}><Center>
           <Tooltip label='Push from clipboard' withArrow>
-            <ActionIcon color='blue' variant='filled' onClick={async () => setFirstText(
-                firstText + await navigator.clipboard.readText(),
-            )}>
+            <ActionIcon color='blue' variant='filled' onClick={async () => {
+              const newText = firstText + await navigator.clipboard.readText();
+              setFirstText(newText);
+              sessionStorage.setItem(SESSION_STORAGE.APP_SIMPLE_DIFF_TEXT1, newText);
+            }}>
               <Clipboard />
             </ActionIcon>
           </Tooltip>
         </Center></Grid.Col>
 
-        <Grid.Col span={1}><Center>
-          <Dropzone onDrop={async (files) => {
-            const result = (await Promise.all(files
-              .map(async (file) => await getBase64(file))))
-              .join('\n');
-            setFirstText(firstText + result);
-          }} openRef={openRef1 as any} style={{ display: 'none' }}>
-            {() => <div>Select files</div>}
-          </Dropzone>
-          <Tooltip label='Select files' withArrow>
-            <ActionIcon
-              color='blue'
-              variant='filled'
-              onClick={() => (openRef1 as any).current()}
-            >
-              <Download />
-            </ActionIcon>
-          </Tooltip>
-        </Center></Grid.Col>
+          <Grid.Col span={1}><Center>
+            <Dropzone onDrop={async (files) => {
+              const result = (await Promise.all(files
+                .map(async (file) => await getBase64(file))))
+                .join('\n');
+              setFirstText(firstText + result);
+              sessionStorage.setItem(SESSION_STORAGE.APP_SIMPLE_DIFF_TEXT1, firstText + result);
+            }} openRef={openRef1 as any} style={{ display: 'none' }}>
+              {() => <div>Select files</div>}
+            </Dropzone>
+            <Tooltip label='Select files' withArrow>
+              <ActionIcon
+                color='blue'
+                variant='filled'
+                onClick={() => (openRef1 as any).current()}
+              >
+                <Download />
+              </ActionIcon>
+            </Tooltip>
+          </Center></Grid.Col>
 
-        <Grid.Col span={1}><Center>
-          <Tooltip label='Clear' withArrow>
-            <ActionIcon color='red' variant='filled' onClick={() => setFirstText('')}>
-              <Eraser />
-            </ActionIcon>
-          </Tooltip>
-        </Center></Grid.Col>
+          <Grid.Col span={1}><Center>
+            <Tooltip label='Clear' withArrow>
+              <ActionIcon color='red' variant='filled' onClick={() => {
+                setFirstText('');
+                sessionStorage.setItem(SESSION_STORAGE.APP_SIMPLE_DIFF_TEXT1, '');
+              }}>
+                <Eraser />
+              </ActionIcon>
+            </Tooltip>
+          </Center></Grid.Col>
 
-        <Grid.Col span={3} offset={1.5}><Center>
-          <Button color={'green'} onClick={() => {
-            const differences = (new Greedy()).differences(firstText, secondText);
-            const newHighlights: string[] = [];
-            (differences).forEach(({ type, value }: { type: string, value: string }) => {
-              if (type !== 'eql') return;
-              if (value.split(/\s/).length < 8) return;
-              newHighlights.push(value);
-            });
-            setHighlight(newHighlights);
-            setIsCompared(true);
-          }}>
-            Compare
-          </Button>
-        </Center></Grid.Col>
+          <Grid.Col span={3} offset={1.5}><Center>
+            <Button color={'green'} onClick={() => {
+              const differences = (new Greedy()).differences(firstText, secondText);
+              const newHighlights: string[] = [];
+              (differences).forEach(({ type, value }: { type: string, value: string }) => {
+                if (type !== 'eql') return;
+                if (value.split(/\s/).length < 8) return;
+                newHighlights.push(value);
+              });
+              setHighlight(newHighlights);
+              setIsCompared(true);
+            }}>
+              Compare
+            </Button>
+          </Center></Grid.Col>
 
-        <Grid.Col span={1} offset={1.5}><Center>
-          <Tooltip label='Push from clipboard' withArrow>
-            <ActionIcon color='blue' variant='filled' onClick={async () => setSecondText(
-                secondText + await navigator.clipboard.readText(),
-            )}>
-              <Clipboard />
-            </ActionIcon>
-          </Tooltip>
-        </Center></Grid.Col>
+          <Grid.Col span={1} offset={1.5}><Center>
+            <Tooltip label='Push from clipboard' withArrow>
+              <ActionIcon color='blue' variant='filled' onClick={async () => {
+                const newText = secondText + await navigator.clipboard.readText();
+                setSecondText(newText);
+                sessionStorage.setItem(SESSION_STORAGE.APP_SIMPLE_DIFF_TEXT2, newText);
+              }}>
+                <Clipboard />
+              </ActionIcon>
+            </Tooltip>
+          </Center></Grid.Col>
 
-        <Grid.Col span={1}><Center>
-          <Dropzone onDrop={async (files) => {
-            const result = (await Promise.all(files
-              .map(async (file) => await getBase64(file))))
-              .join('\n');
-            setSecondText(secondText + result);
-          }} openRef={openRef2 as any} style={{ display: 'none' }}>
-            {() => <div>Select files</div>}
-          </Dropzone>
-          <Tooltip label='Select files' withArrow>
-            <ActionIcon
-              color='blue'
-              variant='filled'
-              onClick={() => (openRef2 as any).current()}
-            >
-              <Download />
-            </ActionIcon>
-          </Tooltip>
-        </Center></Grid.Col>
+          <Grid.Col span={1}><Center>
+            <Dropzone onDrop={async (files) => {
+              const result = (await Promise.all(files
+                .map(async (file) => await getBase64(file))))
+                .join('\n');
+              setSecondText(secondText + result);
+              sessionStorage.setItem(SESSION_STORAGE.APP_SIMPLE_DIFF_TEXT2, secondText + result);
+            }} openRef={openRef2 as any} style={{ display: 'none' }}>
+              {() => <div>Select files</div>}
+            </Dropzone>
+            <Tooltip label='Select files' withArrow>
+              <ActionIcon
+                color='blue'
+                variant='filled'
+                onClick={() => (openRef2 as any).current()}
+              >
+                <Download />
+              </ActionIcon>
+            </Tooltip>
+          </Center></Grid.Col>
 
-        <Grid.Col span={1}><Center>
-          <Tooltip label='Clear' withArrow>
-            <ActionIcon color='red' variant='filled' onClick={() => setSecondText('')}>
-              <Eraser />
-            </ActionIcon>
-          </Tooltip>
-        </Center></Grid.Col>
+          <Grid.Col span={1}><Center>
+            <Tooltip label='Clear' withArrow>
+              <ActionIcon color='red' variant='filled' onClick={() => {
+                setSecondText('');
+                sessionStorage.setItem(SESSION_STORAGE.APP_SIMPLE_DIFF_TEXT2, '');
+              }}>
+                <Eraser />
+              </ActionIcon>
+            </Tooltip>
+          </Center></Grid.Col></>
+      }
 
       <Grid.Col span={6}>
         {isCompared ?
           <Highlight
-            highlight={highlight} onClick={() => isCompared && setIsCompared(false)}
+            highlight={highlight}
+            onClick={() => isCompared && setIsCompared(false)}
+            color={theme.colorScheme === APP_THEMES.DARK ? '#fff' : '#000'}
           >{firstText}</Highlight>
           :
           <Textarea
@@ -176,14 +224,19 @@ export const SimpleDifference = ({ parsedCookie }: any) => {
             value={firstText}
             minRows={15}
             maxRows={15}
-            onChange={(event) => setFirstText(event.currentTarget.value)}
+            onChange={(event) => {
+              setFirstText(event.currentTarget.value);
+              sessionStorage.setItem(SESSION_STORAGE.APP_SIMPLE_DIFF_TEXT1, event.currentTarget.value);
+            }}
           />
         }
       </Grid.Col>
       <Grid.Col span={6}>
         {isCompared ?
           <Highlight
-            highlight={highlight} onClick={() => isCompared && setIsCompared(false)}
+            highlight={highlight}
+            onClick={() => isCompared && setIsCompared(false)}
+            color={theme.colorScheme === APP_THEMES.DARK ? '#fff' : '#000'}
           >{secondText}</Highlight>
           :
           <Textarea
@@ -192,7 +245,10 @@ export const SimpleDifference = ({ parsedCookie }: any) => {
             autosize
             minRows={15}
             maxRows={15}
-            onChange={(event) => setSecondText(event.currentTarget.value)}
+            onChange={(event) => {
+              setSecondText(event.currentTarget.value);
+              sessionStorage.setItem(SESSION_STORAGE.APP_SIMPLE_DIFF_TEXT2, event.currentTarget.value);
+            }}
           />
         }
       </Grid.Col>
